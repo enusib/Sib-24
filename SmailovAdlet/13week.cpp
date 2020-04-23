42ec405fc3569848436bbaf8be2e8be383f74445
enusib <63464546+enusib@users.noreply.github.com>
2020-04-23 18:46:52 +0600

    Update 13week.cpp

--- SmailovAdlet/13week.cpp
+++ SmailovAdlet/13week.cpp
@@ -1,18 +1,22 @@
 //Вычисление НОД больших чисел
-
 #include <iostream>
+
 #include <gmpxx.h>
 
 int main() {
-mpz_class a("723132135121");
-mpz_class b("548685452157");
-mpz_class c;
-while(b!=0)
-{
-c=b;
-b=a%b;
-a=c;
-}
-std::cout<<"Answer is " << a << "\n";
-return 0;
+    mpz_class a;
+    mpz_class b;
+    mpz_class c;
+    std::cout << "Calculation of GCD numbers\n";
+    std::cout << "Enter the first number = \n";
+    std::cin >> a;
+    std::cout << "Enter the second number = \n";
+    std::cin >> b;
+    while (b != 0) {
+        c = b;
+        b = a % b;
+        a = c;
+    }
+    std::cout << "The GCD of numbers is " << a << "\n";
+    return 0;
 }
