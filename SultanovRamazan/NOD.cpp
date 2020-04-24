050d20a7a832224665f985619e603b80643e03ea
enusib <63464546+enusib@users.noreply.github.com>
2020-04-24 17:20:46 +0500

    Update NOD.cpp

--- SultanovRamazan/NOD.cpp
+++ SultanovRamazan/NOD.cpp
@@ -6,20 +6,22 @@ int main() {
 mpz_class a;
 mpz_class b;
 
-std::cout<<"a = ";
-std::cin>>a;
-std::cout<<"\nb = ";
-std::cin>>b;
+std:: cout << "a = ";
+std:: cin >> a;
+std:: cout << "\nb = ";
+std:: cin >> b;
 
 mpz_class i;
-i=a;
+i = a;
 
-while(!(a % i == 0 && b % i == 0)){
-if (i>0) i--; 
+while (!(a % i == 0 && b % i == 0))
+{
+if ( i>0 ) 
+  i--; 
 else break;
 }
 
-std::cout << "\nNOD = " << i << "\nSultanov Ramazan (SIB-24)\n";
+std:: cout << "\nNOD = " << i << "\nSultanov Ramazan (SIB-24)\n";
 
 return 0;
 }
