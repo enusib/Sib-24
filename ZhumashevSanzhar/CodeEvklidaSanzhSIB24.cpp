c4f22488cc73c1cf50658d58716e7b72a866c48f
enusib <63464546+enusib@users.noreply.github.com>
2020-04-24 18:00:18 +0600

    Update and rename CodeEvklidaSanzhSIB24 to CodeEvklidaSanzhSIB24.cpp

---
+++ ZhumashevSanzhar/CodeEvklidaSanzhSIB24.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+#include <gmpxx.h>
+using namespace std;
+mpz_class gcd(mpz_class x, mpz_class y) {
+	std::cout<<"GCD of numbers\n";
+	mpz_class z;
+	std::cout <<"enter x = \n";
+ 	std::cin >> x;
+	std::cout << "enter y = \n";
+ 	std::cin >> y;
+ 	while(y!=0) {
+	z=y;
+	y=x%y;
+	x=z; }
+	return x; 
+}
+
+int main() {
+  mpz_class x;
+  mpz_class y;
+  std::cout<<gcd(x,y) << " is GCD of numbers"<<"\n";
+}
