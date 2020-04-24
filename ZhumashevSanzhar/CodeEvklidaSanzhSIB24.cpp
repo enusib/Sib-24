6841c4eb210c60018b18f2d4d0c0e37bbb3ce74e
enusib <63464546+enusib@users.noreply.github.com>
2020-04-24 18:22:30 +0600

    Update CodeEvklidaSanzhSIB24.cpp

--- ZhumashevSanzhar/CodeEvklidaSanzhSIB24.cpp
+++ ZhumashevSanzhar/CodeEvklidaSanzhSIB24.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
 #include <math.h>
 #include <gmpxx.h>
+
 using namespace std;
+
 mpz_class gcd(mpz_class x, mpz_class y) {
-	std::cout<<"GCD of numbers\n";
-	mpz_class z;
-	std::cout <<"enter x = \n";
- 	std::cin >> x;
-	std::cout << "enter y = \n";
- 	std::cin >> y;
- 	while(y!=0) {
-	z=y;
-	y=x%y;
-	x=z; }
-	return x; 
+    std::cout << "GCD of numbers\n";
+    mpz_class z;
+    std::cout << "enter x = \n";
+    std::cin >> x;
+    std::cout << "enter y = \n";
+    std::cin >> y;
+    while (y != 0) {
+        z = y;
+        y = x % y;
+        x = z;
+    }
+    return x;
 }
 
 int main() {
-  mpz_class x;
-  mpz_class y;
-  std::cout<<gcd(x,y) << " is GCD of numbers"<<"\n";
+    mpz_class x;
+    mpz_class y;
+    std::cout << gcd(x, y) << " is GCD of numbers" << "\n";
 }
